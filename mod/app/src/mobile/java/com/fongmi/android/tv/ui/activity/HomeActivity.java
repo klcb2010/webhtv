d850948e724122e07f916f1135139fc7d4eda50e
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-08-29 07:18:47 +0800

    Update HomeActivity.java

--- mod/app/src/mobile/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ mod/app/src/mobile/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.fongmi.android.tv.ui.activity;
 
-import android.tv.setting.AutoBackupPolicy;
 import android.app.PendingIntent;
 import android.app.SearchManager;
 import android.content.Intent;
