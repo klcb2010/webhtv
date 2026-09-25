3015f027b9e13d14bfa485723643e6dca9c2d5c0
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-09-25 17:49:32 +0800

    fix: remove unavailable EventIndex from Startup

--- app/src/main/java/com/fongmi/android/tv/Startup.java
+++ app/src/main/java/com/fongmi/android/tv/Startup.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.startup.Initializer;
 
-import com.fongmi.android.tv.event.EventIndex;
 import com.fongmi.android.tv.setting.Setting;
 import com.fongmi.android.tv.player.mpv.PlaybackRecoveryMonitor;
 import com.fongmi.android.tv.ui.activity.CrashActivity;
@@ -30,7 +29,7 @@ public class Startup implements Initializer<Void> {
         if (PlaybackRecoveryMonitor.isRecoveryProcess(context)) return null;
         CaocConfig.Builder.create().trackActivities(true).backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT).errorActivity(CrashActivity.class).apply();
         Logger.addLogAdapter(new AndroidLogAdapter(PrettyFormatStrategy.newBuilder().methodCount(0).showThreadInfo(false).tag("TV").build()));
-        EventBus.builder().addIndex(new EventIndex()).installDefaultEventBus();
+        EventBus.installDefaultEventBus();
         OkHttp.dns().setDoh(() -> Doh.objectFrom(Setting.getDoh()));
         return null;
     }
